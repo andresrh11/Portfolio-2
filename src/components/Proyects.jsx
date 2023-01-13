f12f8aa87cadcd1ec61c259827dd986b21b5536a
andresrh11 <andresr10020@gmail.com>
2023-01-13 14:26:50 -0500

    actializacion

--- src/components/Proyects.jsx
+++ src/components/Proyects.jsx
@@ -103,41 +103,6 @@ export default function Proyectos() {
           </div>
         </div>
       </section>
-      <section>
-        <div className="hero min-h-screen bg-base-200">
-          <div className="hero-content flex-col lg:flex-row-reverse">
-            <img src={hs} className="max-w-auto rounded-lg " alt="hhs" />
-            <div>
-              <h1 className="text-5xl font-bold">HenryShops!</h1>
-              <p className="py-6 font-normal text-lg">
-                Proyecto grupal elaborado para el bootcamp de SoyHenry, donde
-                nos integramos un grupo de 7 estudiantes para llevar a la
-                realidad esta propuesta que integra tecnologías como:
-                TypeScript, React.js, Styled-components, HTML, CSS, Postgres,
-                Node.js, Redux-toolkit, entre otras. HenryShops tiene como
-                finalidad, permitirle a los pequeños comercios crear su propia
-                pagina web, llegar de una manera diferente y eficaz a sus
-                clientes y manejar de manera optima sus pedidos a traves de
-                Whatsapp.
-              </p>
-              <a
-                href="https://henry-pf.vercel.app/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <button className="btn btn-primary">Sitio</button>
-              </a>
-              <a
-                href="https://github.com/andresrh11/HenryShops"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <button className="btn btn-primary m-2">Codigo</button>
-              </a>
-            </div>
-          </div>
-        </div>
-      </section>
     </>
   );
 }
